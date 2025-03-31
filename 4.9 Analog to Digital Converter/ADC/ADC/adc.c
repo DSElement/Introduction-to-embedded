b5415156d67d8554e42cd08c984a5d15e75acc06
Ghincul Dan <fieldrats1@gmail.com>
2025-03-31 12:52:13 +0300

    finally working

--- 4.9 Analog to Digital Converter/ADC/ADC/adc.c
+++ 4.9 Analog to Digital Converter/ADC/ADC/adc.c
@@ -4,20 +4,19 @@
 
 void ADC_Init(void) {
 	DDRA &= ~(1 << 0);		 //set as input
-    ADMUX = (1 << REFS0);    //select channel
-    ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0);  //set prescalers and aden
+    ADMUX |= (1 << REFS0);    //select channel
+    ADCSRA |= (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0);  //set prescalers and aden
 }
 
 uint32_t ADC_Read() {
     uint32_t result = 0;
 	ADCSRA |= (1 << ADSC); //start conversion
-	while((ADCSRA >> ADSC ) & 1); //wait for conversion to be comlpeted
+	while (ADCSRA & (1 << ADSC)); //wait for conversion to be comlpeted
     result = ADCL;
     result |= (ADCH << 8);
     return result;
 }
 
 uint32_t adc_to_mv(uint32_t adc_value, uint8_t resolution) {
-    //return (adc_value * 5000) >> resolution; // Convert to mV (assuming Vref = 5V)
-	return (adc_value * 5000) / ((1 << resolution)-1);
+    return ((uint32_t)adc_value * 5000) >> resolution; // Convert to mV (assuming Vref = 5V)
 }
--- 4.9 Analog to Digital Converter/ADC/ADC/main.c
+++ 4.9 Analog to Digital Converter/ADC/ADC/main.c
@@ -10,15 +10,10 @@ int main() {
 	char buffer[20];
     while (1) {
         uint32_t adc_value = ADC_Read();
-		uart_transmit(adc_value);
-		_delay_ms(500);
-		//sprintf(buffer, "VOLTAGE = %u mV\r\n", adc_value);
-		//uart_transmit_string(buffer);
-		/*
-		uint16_t voltage = adc_to_mv(adc_value, 10);
+		uint32_t voltage = adc_to_mv(adc_value, 10);
 		sprintf(buffer, "VOLTAGE = %u mV\r\n", voltage);
 		uart_transmit_string(buffer);
-        _delay_ms(1000); // Wait 1 second*/
+		_delay_ms(500);
     }
     
     return 0;
