744d2368024d4ff17c56614e5a08e69c8410d874
Ghincul Dan <fieldrats1@gmail.com>
2025-05-26 14:08:53 +0300

    echo program

---
+++ LPC214x/4.6 UART/main.c
@@ -0,0 +1,55 @@
+#include<lpc214x.h>
+
+/*void pll()
+{
+  PLL0CON=0X01;
+  PLL0CFG=0X24;
+  PLL0FEED=0XAA;
+  PLL0FEED=0X55;
+  while((PLL0STAT&(1<<10))==0);
+  PLL0CON=0X03;
+  PLL0FEED=0XAA;
+  PLL0FEED=0X55;
+  VPBDIV=0x01;             //pclk=60mhz
+}*/
+
+void ser_int()
+{
+  PINSEL0=0X5;
+  U0LCR=0X83;
+  U0DLL=135;
+  U0DLM=1;
+  U0LCR=0X03;
+}
+
+/*void ser_int()
+{
+  PINSEL0=0X5;
+  U0LCR=0X83;
+  U0DLL=0x4F;
+  U0DLM=0;
+  U0LCR=0X03;
+}*/
+
+void tx(unsigned char c)
+{
+  while((U0LSR&(1<<5))==0);
+  U0THR=c;
+}
+
+char rx()
+{
+  while( (U0LSR & 0x01) == 0);	/*Wait till RDR bit becomes 1 which tells that receiver contains valid data */
+	return U0RBR;
+}
+
+int main(void)
+{
+  unsigned char b;
+  //pll();
+  ser_int();
+  while(1) {
+    b = rx();
+		tx(b);
+  }
+}
