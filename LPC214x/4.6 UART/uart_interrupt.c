e9af2db8b9ac1eb663219b04e8d77677f09ab01c
Ghincul Dan <158288362+DSElement@users.noreply.github.com>
2025-06-02 10:59:39 +0300

    Update uart_interrupt.c

--- LPC214x/4.6 UART/uart_interrupt.c
+++ LPC214x/4.6 UART/uart_interrupt.c
@@ -6,13 +6,11 @@ void ser_init()  //serial innit
 {
   PINSEL0=0X5;
   U0LCR=0X83;  // 1 stop bit, 8-bit character length
-  U0DLL=135;  //din formula asa iasa
-  U0DLM=1;		//
+	U0DLL=98;  //pentru 15Mhz
+	U0DLM = 0;
   U0LCR=0X03;  //(Divisor Latch Access Bit)DLAB = 0 (lock baud rate registers)
-	U0IER = 0x00000003;	/* Enable THRE and RBR interrupt */
+	U0IER = 0x00000001;	/* Enable only RBR interrupt, not THRE */
 }
-
-
 
 //Transmitter Holding Register Empty (THRE)
 //THRE is set immediately upon detection of an empty UART0 THR and is
@@ -38,26 +36,24 @@ int main(void)
 	ser_init();
 	VICVectAddr0 = (unsigned) UART0_Interrupt;	// UART0 interrupt service routine address
 	VICVectCntl0 = 0x00000026;	//interrupt source with index 6 (UART0) is enabled as the one with priority 0 (the highest)
-	/*
-	bit 5 - IRQslot_en  - When 1, this vectored IRQ slot is enabled, and can produce a unique ISR
-	address when its assigned interrupt request or software interrupt is enabled,
-	classified as IRQ, and asserted.
-	*/
+	
+	//bit 5 - IRQslot_en  - When 1, this vectored IRQ slot is enabled, and can produce a unique ISR
+	//address when its assigned interrupt request or software interrupt is enabled,
+	//classified as IRQ, and asserted.
 	
 	VICIntEnable = 0x00000040;	// UART0 corresponds to bit 6: 1 << 6 = 0x40
-	/*
-	Interrupt Enable Register. This register controls which of the
-	32 interrupt requests and software interrupts are enabled to
-	contribute to FIQ or IRQ
-	*/
+
+	//Interrupt Enable Register. This register controls which of the
+	//32 interrupt requests and software interrupts are enabled to
+	//contribute to FIQ or IRQ
+	
 	
 	//IRQ - Interrupt Request
 	//FRQ - Fast Interrupt Request
-	VICIntSelect = 0x00000000;	/* UART0 configured as IRQ */
-	/*
-	Interrupt Select Register. This register classifies each of the
-	32 interrupt requests as contributing to FIQ or IRQ
-	*/
+	VICIntSelect = 0x00000000;	// UART0 configured as IRQ
+
+	//Interrupt Select Register. This register classifies each of the
+	//32 interrupt requests as contributing to FIQ or IRQ
+
 	while(1);
 }
-
